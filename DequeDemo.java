f5c5363b368bebd776cb2b2c5df0e9cd00751679
vishnu <vishnuchilakala444@gmail.com>
2026-02-28 18:04:37 +0530

    Added new topics

---
+++ DequeDemo.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class DequeDemo {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new ArrayDeque<>();
+    }
+}
--- PriorityQueueDemo.java
+++ PriorityQueueDemo.java
@@ -2,6 +2,33 @@ import java.util.*;
 
 public class PriorityQueueDemo {
     public static void main(String[] args) {
-        Queue<Integer> q1 = new PriorityQueue<>();
+        Queue<String> q1 = new PriorityQueue<>(); 
+        // Maintains natural ordering
+        // Internally uses Min Heap (Smallest will always be at head)
+        // String are stored in lexicographical order
+        // Numbers are stroed in natural order
+
+        q1.add("Wednesday");
+        q1.add("Monday");
+        q1.add("Tuesday");
+
+        System.out.println(q1);
+
+        while(!q1.isEmpty()) System.out.println(q1.poll());
+
+        System.out.println();
+
+        Queue<Integer> q2 = new PriorityQueue<>();
+        q2.add(30);
+        q2.add(10);
+        q2.add(40);
+        q2.add(20);
+        q2.add(50);
+
+        System.out.println(q2);
+
+        while(!q2.isEmpty()){
+            System.out.println(q2.poll());
+        }
     }
 }
